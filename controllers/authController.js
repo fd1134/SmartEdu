c033875a1d82673d0adc21e3ef16c88ee57cfdee
Fatih Demir <fd1134@gmail.com>
2024-07-31 23:16:14 +0300

    login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const bcrypt =require("bcrypt");
 const User = require("../models/User");
 exports.createUser = async (req, res) => {
   try {
@@ -13,3 +14,19 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+exports.loginUser= async (req,res)=>{
+  const {email,password}=req.body;
+  const user= await User.findOne({email});
+  if(user)
+  {
+    bcrypt.compare(password,user.password,(err,same)=>{
+      if(same){
+        //User Sesion 
+        res.status(200).send("You Are Logged In");
+      }
+    });
+  }
+  
+};
+
